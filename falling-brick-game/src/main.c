72e8058f12d5dd8b20775afec13be0cb6a736fa9
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-06-28 19:16:05 +0700

    Update main.c

--- falling-brick-game/src/main.c
+++ falling-brick-game/src/main.c
@@ -18,8 +18,10 @@ SDL_Rect e_srcR, e_destR[100];
 static SDL_Event event;
 int counter;
 int r_cnt = 0;
-int int_w = 45;
-int int_h = 45;
+int int_w_rock = 45;
+int int_h_rock = 45;
+int int_w_car = 90;
+int int_h_car = 90;
 bool mov_rock = false;
 int ind_mov_rock = -1;
 int rock_dir = 1;
@@ -34,11 +36,11 @@ void clean(void);
 void level_up(void);
 bool running(void) { return isRunning; }
 bool check_rocks(int r, int x);
+void change_txt(int ind);
 
 
+int main( int argc, char* args[] ) {
 
-int main( int argc, char* args[] )
-{
 	(void) argc;
 	(void) args;
 
@@ -89,6 +91,7 @@ int main( int argc, char* args[] )
 
 
 void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen){
+
 	int flags = 0;
 	//check for full screen
 	if(fullscreen){
@@ -117,9 +120,11 @@ void init(const char* title, int xpos, int ypos, int width, int height, bool ful
 	//loading textures from assets folder
 	playertex = LoadTexture("assets/player.bmp", renderer);
 	enemytex = LoadTexture("assets/enemy.bmp", renderer);
+
 }
 
 void handleEvents(void){
+
 	SDL_PollEvent(&event);
 	switch(event.type){
 		case SDL_QUIT:
@@ -128,13 +133,14 @@ void handleEvents(void){
 		default:
 			break;
 	}
+
 }
 
 void update(void){
 
 	if(p_destR.x >= 700) level_up();
 
-	if(mov_rock && ( e_destR[ind_mov_rock].y >= 500 || e_destR[ind_mov_rock].y <= 50 ) ) rock_dir *= -1;
+	if(mov_rock && ( e_destR[ind_mov_rock].y >= 600 || e_destR[ind_mov_rock].y <= 0 ) ) rock_dir *= -1;
 
 	//check the movement of the player
 	if(event.type == SDL_KEYDOWN && event.key.repeat == 0) {
@@ -166,31 +172,38 @@ void update(void){
 			printf("\t*Game Over!*\n\n");
 			isRunning = false;
 		}
+
 	}
 
 }
 
 void render(void){
+
 	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer, playertex, NULL, &p_destR);
 
 	for(int i = 1; i <= r_cnt; i++) {
-		SDL_RenderCopy(renderer, enemytex, NULL, &e_destR[i]);
+		if(mov_rock && ind_mov_rock == i) SDL_RenderCopy(renderer, playertex, NULL, &e_destR[i]);
+		else SDL_RenderCopy(renderer, enemytex, NULL, &e_destR[i]);
 	}
 
 	SDL_RenderPresent(renderer);
+
 }
 
 //cleaning the scren
 void clean(void){
+
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
 	printf("Game cleaned!\n");
+
 }
 
 //texture manager
 static SDL_Texture* LoadTexture(const char* fileName, SDL_Renderer* ren) {
+
 	SDL_Texture* newTexture = NULL;
 	SDL_Surface* loadedSurface = SDL_LoadBMP(fileName);
 	if(loadedSurface == NULL) {
@@ -203,10 +216,12 @@ static SDL_Texture* LoadTexture(const char* fileName, SDL_Renderer* ren) {
 		SDL_FreeSurface(loadedSurface);
 	}
 	return newTexture;
+
 }
 
 //colision detection handler
 static bool AABB(SDL_Rect recA, SDL_Rect recB) {
+
 	if(
 		recA.x + recA.w >= recB.x &&
 		recB.x + recB.w >= recA.x &&
@@ -215,12 +230,13 @@ static bool AABB(SDL_Rect recA, SDL_Rect recB) {
 		return true;
 	}
 	return false;
+
 }
 
 bool check_rocks(int r, int x){
 
-	int int_x = (ScreenWidth + r) / 2 - int_w / 2;
-	int int_y = (ScreenHeight + r) / 1.6 - int_h / 1.6;
+	int int_x = (ScreenWidth + r) / 2 - int_w_rock / 2;
+	int int_y = (ScreenHeight + r) / 1.6 - int_h_rock / 1.6;
 
 	for(int i = x - 1; i >= 1; i--) {
 
@@ -233,9 +249,17 @@ bool check_rocks(int r, int x){
 
 }
 
+void change_txt(int ind){
+
+	e_destR[ind].w = int_w_car;
+	e_destR[ind].h = int_h_car;
+
+}
+
+
 void level_up(void){
-	p_destR.w = 100;
-	p_destR.h = 100;
+	p_destR.w = int_w_car;
+	p_destR.h = int_h_car;
 	p_destR.x = 0;
 	p_destR.y = ScreenHeight / 2;
 
@@ -243,8 +267,8 @@ void level_up(void){
 	if(r_cnt < 5) r_cnt ++;
 
 	for(int i = 1; i <= r_cnt; i++) {
-		e_destR[i].w = int_w;
-		e_destR[i].h = int_h;
+		e_destR[i].w = int_w_rock;
+		e_destR[i].h = int_h_rock;
 
 
 		int r = ( rand() % 600 ) - 300;
@@ -260,5 +284,10 @@ void level_up(void){
 
 	if(r_cnt == 5) mov_rock = true;
 
-	if(mov_rock) ind_mov_rock = rand() % 5;
+	if(mov_rock) {
+		ind_mov_rock = rand() % 5;
+		change_txt(ind_mov_rock);
+	}
+
 }
+
